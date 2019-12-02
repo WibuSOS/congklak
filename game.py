3ee5fea1425d169f4250b938d3a2c9e874431726
WibuSOS <vincent.joyan@gmail.com>
2019-12-03 01:11:50 +0700

    Outline & data structure

---
+++ game.py
@@ -0,0 +1,28 @@
+from pygame.mixer import *
+import time
+import random
+
+def game(screen,screen_width,screen_height):
+    #Music & sound effects
+        #your codes here
+    
+    #Background
+        #your codes here
+    
+    #Sprites
+        #your codes here
+
+    #Buttons & signs
+        #your codes here
+    
+    #Data structure
+    congklak_data = [[["pos1"],"pebbles"],[["pos2"],"pebbles"],[["pos3"],"pebbles"],[["pos4"],"pebbles"],[["pos5"],"pebbles"]
+                    ,[["pos6"],"pebbles"],[["pos7"],"pebbles"],[["pos8"],"pebbles"],[["pos9"],"pebbles"],[["pos10"],"pebbles"]
+                    ,[["pos11"],"pebbles"],[["pos12"],"pebbles"],[["pos13"],"pebbles"],[["pos14"],"pebbles"],[["pos15"],"pebbles"]
+                    ,[["pos16"],"pebbles"]]
+    #entar untuk sprites maybe tiap lubang buat jadi 1 grup, instead of 1 whole big group(?)
+    
+    running = True
+
+    while running:
+        pass
